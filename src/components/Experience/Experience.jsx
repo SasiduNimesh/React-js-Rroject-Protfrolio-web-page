9486d068a7db31bbb33b37b4cf0206bc35949621
SasiduNimesh <117624524+SasiduNimesh@users.noreply.github.com>
2024-06-15 18:12:21 +0530

    Add files via upload

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./experience.css"
+
+const Experience = () => {
+  return (
+    <section className='work-contatiner' id='Experience'>
+      <h2>Work Experience</h2>
+      <div className='work-content'>
+        <div className="job">
+          <h3>Frontend Development</h3>
+          <ul>
+            <li>Developed scalable and responsive web applications using React.js</li>
+            <li>Led a team of developers in implementing complex features and architectural decisions</li>
+            <li>Collaborated with cross-functional teams including designers and backend developers</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h3>Mobile App Design</h3>
+          <ul>
+            <li>Contributed to the development of a large-scale e-commerce platform using AngularJS</li>
+            <li>Optimized website performance and user experience through front-end optimizations</li>
+            <li>Worked closely with product managers to define requirements and prioritize features</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h3>Web Design</h3>
+          <ul>
+            <li>Gained hands-on experience in front-end development technologies such as HTML, CSS, and JavaScript</li>
+            <li>Assisted in the development and maintenance of company websites and web applications</li>
+            <li>Participated in team meetings and contributed to project discussions</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h3>Logo Design</h3>
+          <ul>
+            <li>Designed user interfaces for web and mobile applications using industry-standard design tools</li>
+            <li>Collaborated with developers to ensure seamless integration of design and functionality</li>
+            <li>Conducted user research and usability testing to gather feedback and iterate on designs</li>
+          </ul>
+          </div>
+
+      </div>
+      
+    </section>
+  )
+}
+
+export default Experience;
+
+
+
